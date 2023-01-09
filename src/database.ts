5e60d50b1439784680ad608698ddd2f1c99119fb
JP-Becker <jpbecker20@gmail.com>
2023-01-09 14:21:00 -0300

    exercicios-api-e-express-feitos

--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,5 @@
 import { TUser, TProduct, TPurchase, PRODUCT } from "./types"
 
-export const createUser = (user: TUser) => {
-    users.push(user)
-    console.log('Usuário criado')
-}
-
-export const getAllUsers = () => {
-    const allUsers = users.map((user) => {
-        return user
-    })
-    console.log(allUsers)
-}
-
 export const users: TUser[] = [
     {
         id: '01',
@@ -24,37 +12,6 @@ export const users: TUser[] = [
         password: '123456',
     }
 ]
-
-export const createProduct = (product: TProduct) => {
-    products.push(product)
-
-    console.log("Produto criado com sucesso")
-}
-
-export const getAllProducts = () => {
-    const allProducts = products.map((product) => {
-        return product
-    })
-    console.log(allProducts)
-}
-
-export const getProductById = (idToSearch: string) => {
-    const findProduct = products.find((product) => {
-        return product.id === idToSearch
-    })
-    if (findProduct) {
-        return console.log('O produto que vc buscou é o', findProduct.name)
-    } else {
-        return console.log('Esse produto não existe')
-    }
-}
-
-export const queryProductsByName = (q: string) => {
-    const findProduct = products.find((product) => {
-        return product.name.toLowerCase().includes(q.toLowerCase())
-    })
-    console.log(findProduct)
-}
 
 export const products: TProduct[] = [
     {
@@ -70,20 +27,6 @@ export const products: TProduct[] = [
         category: PRODUCT.FOOD
     }
 ]
-
-export const createPurchase = (purchase: TPurchase) => {
-    purchases.push(purchase)
-
-    console.log("Compra realizada com sucesso")
-}
-
-export const getAllPurchasesFromUserId = (userIdToSearch: string) => {
-    const findPurchase = purchases.find((p) => {
-        return p.userId === userIdToSearch
-    })
-
-    console.log(findPurchase)
-}
 
 export const purchases: TPurchase[] = [
     {
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,99 @@
-import { createUser, getAllUsers, getAllProducts, createProduct, getProductById, queryProductsByName, createPurchase, getAllPurchasesFromUserId } from "./database";
-import { PRODUCT } from "./types";
+import { users, products, purchases } from "./database";
+import { TUser, TProduct, TPurchase } from "./types";
+import  express, { Request, Response} from 'express'
+import cors from 'cors';
 
-createUser({id: '03', email: 'beltrano@gmail.com', password: '1234567'})
-getAllUsers()
 
-createProduct({id: '04', name: 'celular', price: 1399, category: PRODUCT.ELECTRONICS})
-getAllProducts()
-getProductById('04')
-queryProductsByName('CeLular')
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-createPurchase({userId: '03', productId: '04', quantity: 2, totalPrice: 2798})
-getAllPurchasesFromUserId('02')
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+})
+
+// teste
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong')
+})
+
+// get all users
+app.get('/users', (req: Request, res: Response) => {
+    res.status(200).send(users)
+})
+
+// get all products
+app.get('/products', (req: Request, res: Response) => {
+    res.status(200).send(products)
+})
+
+// get product by name
+app.get('/products/search', (req: Request, res: Response) => {
+    const q = req.query.q as string
+    const productsFiltered = products.filter((product) => {
+        return product.name.toLowerCase().includes(q.toLowerCase())
+    })
+
+    res.status(200).send(productsFiltered)
+})
+
+//Create user
+app.post('/users', (req: Request, res: Response) => {
+    const id = req.body.id
+    const email = req.body.email
+    const password = req.body.password
+
+    const newUser: TUser = {
+        id: id,
+        email: email,
+        password: password
+    }
+
+    users.push(newUser)
+
+    res.status(201).send("Usuário criado com sucesso!")
+})
+
+// create product
+app.post('/products', (req: Request, res: Response) => {
+    const id = req.body.id
+    const name = req.body.name
+    const price = req.body.price
+    const category = req.body.category
+
+    const newProduct: TProduct = {
+        id: id,
+        name: name,
+        price: price,
+        category: category
+    }
+
+    products.push(newProduct)
+
+    res.status(201).send("Produto cadastrado com sucesso!")
+})
+
+// get all purchases
+app.get("/purchases", (req: Request, res: Response) => {
+    res.status(200).send(purchases)
+})
+
+// create purchase
+app.post('/purchases', (req: Request, res: Response) => {
+    const userId = req.body.userId
+    const productId = req.body.productId
+    const quantity = req.body.quantity
+    const totalPrice = req.body.totalPrice
+
+    const newPurchase: TPurchase = {
+        userId: userId,
+        productId: productId,
+        quantity: quantity,
+        totalPrice: totalPrice
+    }
+
+    purchases.push(newPurchase)
+
+    res.status(201).send("Compra realizada com sucesso!")
+})
+
